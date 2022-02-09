9cef55c91d77af3ffa53b1a1c70023064edac7b6
abetusk <abetusk@gmail.com>
2022-02-09 11:33:49 -0500

    wip

--- sandbox/wip-vadcrzr/script.js
+++ sandbox/wip-vadcrzr/script.js
@@ -21,18 +21,115 @@
 
 var g_info = {
   "VERSION" : "0.1.0",
-  "download_filename": "BOILERPLATE.png",
+  "download_filename": "vadcrzr.png",
   "canvas": {},
   "ctx" : {},
   "ready": false,
   "tick" : 0,
   "tick_val" : 0,
 
-  "fps_debug": false,
+  "fps_debug": true,
   "fps": 0,
   "last_t":0,
 
+  "alphabet": {
+    "a": [ "xxx" + "x.x" + "xxx" + "x.x" + "x.x" ],
+    "b": [ "xxx" + "x.x" + "xxx" + "x.x" + "xxx" ],
+    "c": [ "xxx" + "x.." + "x.." + "x.." + "xxx" ],
+    "d": [ "xx." + "x.x" + "x.x" + "x.x" + "xx." ],
+    "e": [ "xxx" + "x.." + "xx." + "x.." + "xxx" ],
+    "f": [ "xxx" + "x.." + "xx." + "x.." + "x.." ],
+    "g": [ "xxx" + "x.." + "x.." + "x.x" + "xxx" ],
+    "h": [ "x.x" + "x.x" + "xxx" + "x.x" + "x.x" ],
+    "i": [ "xxx" + ".x." + ".x." + ".x." + "xxx" ],
+    "j": [ "xxx" + "..x" + "..x" + "x.x" + "xxx" ],
+    "k": [ "x.x" + "xx." + "x.." + "xx." + "x.x" ],
+    "l": [ "x.." + "x.." + "x.." + "x.." + "xxx" ],
+    "m": [ "x.x" + "xxx" + "x.x" + "x.x" + "x.x" ],
+    "n": [ "xxx" + "x.x" + "x.x" + "x.x" + "x.x" ],
+    "o": [ "xxx" + "x.x" + "x.x" + "x.x" + "xxx" ],
+    "p": [ "xxx" + "x.x" + "xxx" + "x.." + "x.." ],
+    "q": [ "xxx" + "x.x" + "x.x" + "x.x" + "xx." ],
+    "r": [ "xxx" + "x.x" + "xx." + "xx." + "x.x" ],
+    "s": [ "xxx" + "x.." + "xxx" + "..x" + "xxx" ],
+    "t": [ "xxx" + ".x." + ".x." + ".x." + ".x." ],
+    "u": [ "x.x" + "x.x" + "x.x" + "x.x" + "xxx" ],
+    "v": [ "x.x" + "x.x" + "x.x" + "x.x" + ".x." ],
+    "w": [ "x.x" + "x.x" + "x.x" + "xxx" + "x.x" ],
+    "x": [ "x.x" + "x.x" + ".x." + "x.x" + "x.x" ],
+    "y": [ "x.x" + "x.x" + "xxx" + ".x." + ".x." ],
+    "z": [ "xxx" + "..x" + ".x." + "x.." + "xxx" ]
+  },
+
   "palette": [
+  {
+    "name": "nowak",
+    "colors": ["#e85b30", "#ef9e28", "#c6ac71", "#e0c191", "#3f6279", "#ee854e", "#180305"],
+    "stroke": "#180305",
+    "background": "#ede4cb"
+  },
+  {
+    "name": "dt09",
+    "colors": ["#052e57", "#de8d80"],
+    "stroke": "#efebda",
+    "background": "#efebda"
+  },
+  {
+    "name": "present-correct",
+    "colors": [ "#fd3741", "#fe4f11", "#ff6800", "#ffa61a", "#ffc219", "#ffd114", "#fcd82e", "#f4d730",
+                "#ced562", "#8ac38f", "#79b7a0", "#72b5b1", "#5b9bae", "#6ba1b7", "#49619d", "#604791",
+                "#721e7f", "#9b2b77", "#ab2562", "#ca2847" ]
+  },
+  {
+    "name": "tundra2",
+    "colors": ["#5f9e93", "#3d3638", "#733632", "#b66239", "#b0a1a4", "#e3dad2"]
+  },
+  {
+    "name": "roygbiv-warm",
+    "colors": [ "#705f84", "#687d99", "#6c843e", "#fc9a1a", "#dc383a", "#aa3a33", "#9c4257" ]
+  },
+  {
+    "name": "jungle",
+    "colors": [ "#adb100", "#e5f4e9", "#f4650f", "#4d6838", "#cb9e00", "#689c7d", "#e2a1a8", "#151c2e" ],
+    "stroke": "#0e0f27",
+    "background": "#cecaa9"
+  },
+  {
+    "name": "ducci_g",
+    "colors": ["#c75669", "#000000", "#11706a"],
+    "stroke": "#11706a",
+    "background": "#ecddc5"
+  },
+  {
+    "name": "rag-taj",
+    "colors": ["#ce565e", "#8e1752", "#f8a100", "#3ac1a6"],
+    "background": "#efdea2"
+  },
+  {
+    "name": "system.#05",
+    "colors": ["#db4549", "#d1e1e1", "#3e6a90", "#2e3853", "#a3c9d3"],
+    "stroke": "#000",
+    "background": "#fff"
+  },
+  {
+    "name": "san ramon",
+    "colors": ["#4f423a", "#f6a74b", "#589286", "#f8e9e2", "#2c2825"],
+    "stroke": "#2c2825",
+    "background": "#fff"
+  },
+  {
+    "name": "dt10",
+    "colors": ["#e5dfcf", "#151513"],
+    "stroke": "#151513",
+    "background": "#e9b500"
+  },
+  {
+    "name": "revolucion",
+    "colors": ["#ed555d", "#fffcc9", "#41b797", "#eda126", "#7b5770"],
+    "stroke": "#fffcc9",
+    "background": "#2d1922"
+  },
+
   {  
     "name": "jrmy",
     "colors": ["#df456c", "#ea6a82", "#270b32", "#471e43"],
@@ -83,8 +180,28 @@ var g_info = {
   "speed_factor":256,
   "color": [ ],
 
+  "state": {
+    "blink_open": false,
+    "blink_cur_t":0,
+    "blink_end_t":0
+  },
+
+  "fg_amp_x": 0,
+  "fg_amp_y": 5,
+  "fg_freq_x": 0,
+  "fg_freq_y": 1/20.0,
+  "fg_phase_x": 0,
+  "fg_phase_y": 0,
+
   "grid": [],
   "grid_mem": {},
+
+  "bg_grid": [],
+  "bg_key":[],
+  "bg_amp": 32.0,
+  "bg_freq": 1/64.0,
+  "bg_phase_x": 0.3,
+  "bg_phase_y": 0.1,
 
   "rnd":[],
 
@@ -170,6 +287,45 @@ function _rgb2hex(r, g, b) {
   }
   return "#" + _tohex(r) + _tohex(g) + _tohex(b);
 }
+
+
+function _hex2rgb(rgb) {
+  let s = 0;
+  let d = 2;
+  if (rgb[0] == '#') {
+    rgb = rgb.slice(1);
+  }
+  if (rgb.length==3) { d = 1; }
+  let hxr = rgb.slice(s,s+d);
+  if (hxr.length==1) { hxr += hxr; }
+  s += d;
+
+  let hxg = rgb.slice(s,s+d);
+  if (hxg.length==1) { hxg += hxg; }
+  s += d;
+
+  let hxb = rgb.slice(s,s+d);
+  if (hxb.length==1) { hxb += hxb; }
+  s += d;
+
+  let v = { "r": parseInt(hxr,16), "g": parseInt(hxg,16), "b": parseInt(hxb,16) };
+  return v;
+}
+
+function _hex2hsv(rgbhex) {
+  let rgb = _hex2rgb(rgbhex);
+  return RGBtoHSV(rgb.r, rgb.g, rgb.b);
+}
+
+function _hex_dhsv(hexstr, dh, ds, dv) {
+  let hsv_c = _hex2hsv(hexstr);
+  hsv_c.h = _clamp(hsv_c.h + dh, 0, 1);
+  hsv_c.s = _clamp(hsv_c.s + ds, 0, 1);
+  hsv_c.v = _clamp(hsv_c.v + dv, 0, 1);
+  let rgb_c = HSVtoRGB(hsv_c.h, hsv_c.s, hsv_c.v);
+  return _rgb2hex(rgb_c.r, rgb_c.g, rgb_c.b);
+}
+
 
 // https://stackoverflow.com/a/596243 CC-BY-SA
 // https://stackoverflow.com/users/61574/anonymous
@@ -312,29 +468,85 @@ function loading_anim() {
 
 }
 
-function disp_r(cx,cy,w,h,g,c) {
+function disp_txt(txt,cx,cy,w,h,c) {
+  let _cw = g_info.canvas.width;
+  let _ch = g_info.canvas.height;
+  let ctx = g_info.ctx;
+
+  for (let i =0; i<txt.length; i++) {
+    if (!(txt[i] in g_info.alphabet)) { continue; }
+
+
+
+
+  }
+
+
+}
+
+function disp_r(cx,cy,w,h,g,c,no_eyes,no_shadow,no_overlap) {
+  no_eyes = ((typeof no_eyes === "undefined") ? false : no_eyes);
+  no_shadow = ((typeof no_shadow === "undefined") ? false : no_shadow);
+  no_overlap = ((typeof no_overlap === "undefined") ? false : no_overlap);
   let _cw = g_info.canvas.width;
   let _ch = g_info.canvas.height;
   let ctx = g_info.ctx;
 
   ds = _min(w/(g[0].length+1),h/(g.length+1));
 
+  let shadow_offset = 4;
+
+  let _o = 0.75;
+  if (no_overlap) { _o = 0; }
+
+  if (!no_shadow) {
+    let _so = shadow_offset;
+    for (let i=0; i<g.length; i++) {
+      for (let j=0; j<g[i].length; j++) {
+        let _x = cx + j*ds + ds/2;
+        let _y = cy + i*ds + ds/2;
+
+        if (g[i][j] > 0) {
+          //ctx.fillStyle = "rgba(32,32,32,0.2)";
+
+          ctx.fillStyle = '#777';
+          ctx.beginPath();
+          ctx.fillRect(_x + _so,_y + _so, ds + _o, ds + _o);
+
+        }
+       
+      }
+    }
+  }
+
+
   for (let i=0; i<g.length; i++) {
     for (let j=0; j<g[i].length; j++) {
       let _x = cx + j*ds + ds/2;
       let _y = cy + i*ds + ds/2;
 
-      if (g[i][j] > 0) {
+      if ((g[i][j] < 0) && (no_eyes)) { continue; }
+
+      if (g[i][j] != 0) {
         //ctx.fillStyle = "rgba(32,32,32,0.2)";
-        ctx.fillStyle = c;
+
+        if (g[i][j] > 0) {
+          ctx.fillStyle = c;
+        }
+        else {
+
+          ctx.fillStyle = "rgba(240,240,240,0.3)";
+        }
+
         ctx.beginPath();
-        ctx.fillRect(_x,_y, ds + 0.75, ds + 0.75);
+        ctx.fillRect(_x,_y, ds + _o, ds + _o);
 
       }
     }
   }
 
 }
+
 
 function anim() {
 
@@ -351,29 +563,123 @@ function anim() {
     return;
   }
 
+  let _base_block_w = g_info.grid[0].length;
+  let _base_block_h = g_info.grid.length;
+
   // PER FRAME CODE
   //
 
-  let ds = _min(_cw/10,_ch/10);
+  let ds = _min(_cw/(_base_block_w+5),_ch/(_base_block_h+5));
 
   let grid = g_info.grid;
 
+  let cx = _cw/2 - g_info.grid[0].length*ds/2;
+  let cy = _ch/2 - g_info.grid.length*ds/2;
+
+  let shadow_offset = 10;
+
+  // background
+  //
+  let pal_n = g_info.palette_choice.colors.length;
+  let walk_idx = 0;
+  let walk_d = Math.floor(g_info.rnd[0]*65537);
+  //let bg_ds = 2*ds/3;
+  //let bgo = ds/8;
+  let bg_seg = 16;
+  let bg_ds = _max(_cw, _ch)/bg_seg;
+  let bgo = 0;
+  for (let i=-3; i<(bg_seg+3); i++) {
+    for (let j=-3; j<(bg_seg+3); j++) {
+      let x = j*bg_ds + bgo/2;
+      let y = i*bg_ds + bgo/2;
+      if ((i%2)==1) {
+        x += bg_ds/2;
+      }
+
+      let _bg_key = g_info.bg_key[ walk_idx ];
+      let pal_c = g_info.palette_choice.colors[ walk_idx % g_info.palette_choice.colors.length ];
+      walk_idx = (walk_idx+walk_d)%g_info.bg_key.length;
+
+      let _bg_grid = g_info.grid_mem[_bg_key];
+
+      let dx = g_info.bg_amp*Math.cos( g_info.bg_freq*g_info.tick + g_info.bg_phase_x );
+      let dy = g_info.bg_amp*Math.sin( g_info.bg_freq*g_info.tick + g_info.bg_phase_y );
+
+      //let c = _hex_dhsv(pal_c, 0, -0.65, -0.65);
+      let rgb = _hex2rgb(pal_c);
+      let c = "rgba(" + rgb.r + "," + rgb.g + "," + rgb.b + ",0.075)";
+
+      //let c = "rgba(100,80,80,0.15)";
+      disp_r(x+dx, y+dy, bg_ds-bgo, bg_ds-bgo, _bg_grid.grid, c, true, true, true);
+    }
+  }
+
+
+  // Monster
+  //
+
+  let dx = g_info.fg_amp_x*Math.cos( g_info.fg_freq_x*g_info.tick + g_info.fg_phase_x );
+  let dy = g_info.fg_amp_y*Math.cos( g_info.fg_freq_y*g_info.tick + g_info.fg_phase_y );
+
+  // shadow
+  //
+  let _so = shadow_offset;
   for (let i=0; i<grid.length; i++) {
     for (let j=0; j<grid[i].length; j++) {
-      let x = 10 + j*ds;
-      let y = 10 + i*ds;
+      let x = cx + j*ds + dx;
+      let y = cy + i*ds + dy;
 
       if (grid[i][j] > 0) {
-        //ctx.fillStyle = "rgba(32,32,32,0.2)";
-        ctx.fillStyle = g_info.palette_choice.colors[0];
+        ctx.fillStyle = '#555';
         ctx.beginPath();
-        ctx.fillRect(x,y, ds + 0.75, ds + 0.75);
+        ctx.fillRect(x + _so,y + _so, ds + 0.75, ds + 0.75);
 
-        let _g = g_info.grid_mem[grid[i][j]].grid;
-        let _c = g_info.grid_mem[grid[i][j]].c;
+      }
 
+    }
+  }
 
-        disp_r(x,y,ds,ds, _g, _c);
+  // main monster
+  //
+  for (let i=0; i<grid.length; i++) {
+    for (let j=0; j<grid[i].length; j++) {
+      let x = cx + j*ds + dx;
+      let y = cy + i*ds + dy;
+
+      if (grid[i][j] != 0) {
+        //ctx.fillStyle = "rgba(32,32,32,0.2)";
+        if (grid[i][j] > 0) {
+          ctx.fillStyle = g_info.palette_choice.colors[0];
+          ctx.beginPath();
+          ctx.fillRect(x,y, ds + 0.75, ds + 0.75);
+
+          let _g = g_info.grid_mem[grid[i][j]].grid;
+          let _c = g_info.grid_mem[grid[i][j]].c;
+
+          disp_r(x,y,ds,ds, _g, _c);
+        }
+        else {
+
+          if (g_info.state.blink_cur_t >= g_info.state.blink_end_t) {
+            g_info.state.blink_cur_t = 0;
+            g_info.state.blink_end_t = _irnd(10, 50);
+            g_info.state.blink_open = !g_info.state.blink_open;
+          }
+          g_info.state.blink_cur_t++;
+
+          if (g_info.state.blink_open) {
+            ctx.fillStyle = "rgba(240,40,40,0.3)";
+          }
+          else {
+            ctx.fillStyle = "rgba(240,240,240,0.13)";
+            //ctx.fillStyle = "rgba(240,240,240,0.5)";
+          }
+            ctx.fillStyle = "rgba(240,240,240,0.13)";
+          ctx.beginPath();
+          ctx.fillRect(x,y, ds , ds );
+
+        }
+
       }
     }
   }
@@ -449,17 +755,26 @@ function init_fin() {
   g_info.grid = gen_vadcrzr();
 
   let g = g_info.grid;
-  let max_idx = _get_max_idx(g);
+  let base_idx = _get_max_idx(g) + 1;
 
   for (let i=0; i<g.length; i++) {
     for (let j=0; j<g[i].length; j++) {
       let idx = g[i][j];
       if (idx == 0) { continue; }
       if (idx in g_info.grid_mem) { continue; }
-      let _rg = gen_vadcrzr(max_idx);
+      let _rg = gen_vadcrzr(base_idx);
       //g_info.grid_mem[idx] = {"grid":_rg, "c": _arnd(g_info.palette_choice.colors) };
       g_info.grid_mem[idx] = {"grid":_rg, "c": _arnd(g_info.palette_choice.colors.slice(1)) };
-      max_idx = _get_max_idx(_rg);
+      base_idx= _get_max_idx(_rg)+1;
+    }
+  }
+
+  g_info.bg_grid = gen_vadcrzr(base_idx)+1;
+
+  for (let key in g_info.grid_mem) {
+    let ikey = parseInt(key);
+    if (ikey>0) {
+      g_info.bg_key.push(ikey);
     }
   }
 
@@ -521,12 +836,16 @@ function gen_vadcrzr(base_idx) {
   ];
 
   let H = [ { "v":6, "p":0.1 }, { "v":7, "p":0.1 }, {"v":8, "p":0.8} ];
-  let W = [ { "v":7, "p":0.1 }, { "v":8, "p":0.4 }, {"v":9, "p":0.2}, {"v":10, "p":0.2}];
+  let W = [ { "v":7, "p":0.05 }, { "v":8, "p":0.3 }, {"v":9, "p":0.3}, {"v":10, "p":0.3}, {"v":11, "p":.05}, {"v":15, "p":1/64.0}];
 
 
+  let fill_corner = ((_rnd() < 0.5) ? true : false);
 
-  let h = H[_irnd(3)].v;
-  let w = W[_irnd(4)].v;
+  //let h = H[_irnd(3)].v;
+  //let w = W[_irnd(4)].v;
+
+  let h = _arnd(H).v;
+  let w = _arnd(W).v;
 
   for (let iy=0; iy<h; iy++) {
     grid.push([]);
@@ -643,7 +962,6 @@ function gen_vadcrzr(base_idx) {
     for (let j=0; j<w; j++) {
       s += grid[i][j];
     }
-    console.log(s);
   }
   return grid;
 }
